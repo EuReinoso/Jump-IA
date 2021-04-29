2ee4240bd5d479f23ad1f24f76338ae1d1a36aa1
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-29 12:09:33 -0300

    Individual more colors and settings

--- individual.py
+++ individual.py
@@ -1,21 +1,32 @@
 import pygame
 from network import Network
+from random import choice
 
 pygame.init()
+
+COLORS = [
+    (200,0,0),
+    (0,200,0),
+    (0,0,200),
+    (200,200,0),
+    (200,0,200),
+    (0,200,200)
+]
 
 class Individual:
     def __init__(self, rect, network_sizes):
         self.rect = rect
         self.limit = rect.y
         self.network = Network(network_sizes)
-        
+
         self.y_momentum = 0
         self.y_vel = 0.2
 
+        self.color = choice(COLORS)
         self.jump_force = 7
 
     def draw(self, window):
-        pygame.draw.rect(window, (0, 0, 200), self.rect)
+        pygame.draw.rect(window, self.color, self.rect)
     
     def gravity(self):
         self.rect.y += self.y_momentum
@@ -23,6 +34,7 @@ class Individual:
 
         if self.rect.y >= self.limit:
             self.y_momentum = 0
+            self.rect.y = self.limit
     
     def jump(self):
         if self.y_momentum == 0:
@@ -35,5 +47,5 @@ class Individual:
 
         output = self.network.feedforward(inputs)
 
-        if output >= 0.5:
+        if output == 1:
             self.jump()
