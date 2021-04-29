42c5ef56b19c828d82ef5fb9cbd6684d0076fbc3
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-29 09:00:38 -0300

    individual jump settings

--- individual.py
+++ individual.py
@@ -7,18 +7,20 @@ class Individual:
         self.rect = rect
         self.limit = rect.y
         
-        self.y_momentum = 10
-        self.y_vel = 0
+        self.y_momentum = 0
+        self.y_vel = 0.2
+
+        self.jump_force = 6
 
     def draw(self, window):
         pygame.draw.rect(window, (0, 0, 200), self.rect)
     
-    def gravity():
-        self.rect.y += self.y_vel
-        self.y_vel += self.y_momentum
+    def gravity(self):
+        self.rect.y += self.y_momentum
+        self.y_momentum += self.y_vel
 
         if self.rect.y >= self.limit:
-            self.y_vel = 0
+            self.y_momentum = 0
     
-    def jump():
-        self.y_vel -= self.y_momentum
+    def jump(self):
+        self.y_momentum -= self.jump_force
