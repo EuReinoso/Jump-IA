039bc7eb4d7ded118bd9c73ad4ca24ed9b57994d
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-29 10:34:41 -0300

    Individual get_action

--- individual.py
+++ individual.py
@@ -1,16 +1,18 @@
 import pygame
+from network import Network
 
 pygame.init()
 
 class Individual:
-    def __init__(self, rect):
+    def __init__(self, rect, network_sizes):
         self.rect = rect
         self.limit = rect.y
+        self.network = Network(network_sizes)
         
         self.y_momentum = 0
         self.y_vel = 0.2
 
-        self.jump_force = 6
+        self.jump_force = 7
 
     def draw(self, window):
         pygame.draw.rect(window, (0, 0, 200), self.rect)
@@ -23,4 +25,15 @@ class Individual:
             self.y_momentum = 0
     
     def jump(self):
-        self.y_momentum -= self.jump_force
+        if self.y_momentum == 0:
+            self.y_momentum -= self.jump_force
+    
+    def get_action(self, inputs):
+        for i in inputs:
+            if i == None:
+                return
+
+        output = self.network.feedforward(inputs)
+
+        if output >= 0.5:
+            self.jump()
