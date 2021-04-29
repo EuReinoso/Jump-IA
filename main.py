4059a70ac1ec598069fd504b2c424f1df936e727
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-29 09:36:51 -0300

    implementing draw blocks and individual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame, sys
+from individual import Individual
+from block import Block
 
 pygame.init()
 
@@ -10,8 +12,40 @@ pygame.display.set_caption('Jump IA')
 ground_size = (WINDOW_SIZE[0], 100)
 ground = pygame.Rect(0, WINDOW_SIZE[1] - ground_size[1], ground_size[0], ground_size[1])
 
+individual_size = (50, 50)
+individual_rect = pygame.Rect(150, ground.y - individual_size[1], individual_size[0], individual_size[1])
+individual = Individual(individual_rect)
+
+block_size = (50, 50)
+block_position = (WINDOW_SIZE[0], ground.y - block_size[1])
+blocks = []
+block_ticks = 0
+
+vel = 10
+
+def gen_blocks():
+    global block_ticks
+    block_ticks += 1
+
+    if block_ticks >= 160:
+        block_ticks = 0
+        blocks.append(Block(block_size, block_position))
+
+def update_blocks():
+    if len(blocks) > 0:
+        for blk in blocks:
+            blk.draw(window)
+            blk.move(10)
+
 def draw():
     pygame.draw.rect(window, (200, 200, 200), ground)
+    update_blocks()
+    individual.draw(window)
+
+
+
+time = pygame.time.Clock()
+fps = 60
 
 while True:
 
@@ -21,6 +55,12 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                individual.jump()
 
     draw()
+    gen_blocks()
+    individual.gravity()
     pygame.display.update()
+    time.tick(fps)
