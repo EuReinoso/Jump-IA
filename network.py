5b58c7794b0edb6995c2e47fcb9e7d581c4a81c8
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-29 12:10:03 -0300

    network threshold activation

--- network.py
+++ network.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 def sigmoid(z):
+    z = np.array(z, dtype= np.float128)
     return 1.0/(1.0 + np.exp(-z))
+
+def threshold(z):
+    if z >= 0:
+        return 1
+    return 0
 
 class Network:
     def __init__(self, sizes):
@@ -12,6 +18,7 @@ class Network:
 
     def feedforward(self, a):
         for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w, a) + b)
+            #a = sigmoid(np.dot(w, a) + b)
+            a = threshold(np.dot(w, a) + b)
         return a
     
