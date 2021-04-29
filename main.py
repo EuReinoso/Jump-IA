100794e6bd3f89945023df9f7f4f6ff6be00f259
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-29 08:30:53 -0300

    draw ground

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ WINDOW_SIZE = (640, 480)
 window = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption('Jump IA')
 
+ground_size = (WINDOW_SIZE[0], 100)
+ground = pygame.Rect(0, WINDOW_SIZE[1] - ground_size[1], ground_size[0], ground_size[1])
+
+def draw():
+    pygame.draw.rect(window, (200, 200, 200), ground)
+
 while True:
 
     window.fill((0, 0, 0))
@@ -14,6 +20,7 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.quit()
+            sys.exit()
 
+    draw()
     pygame.display.update()
