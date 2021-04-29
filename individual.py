362c0f646ecf060abc52292a48b7b8d0c82e25cd
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-29 08:44:37 -0300

    Individual  limit

--- individual.py
+++ individual.py
@@ -5,6 +5,7 @@ pygame.init()
 class Individual:
     def __init__(self, rect):
         self.rect = rect
+        self.limit = rect.y
         
         self.y_momentum = 10
         self.y_vel = 0
@@ -12,11 +13,11 @@ class Individual:
     def draw(self, window):
         pygame.draw.rect(window, (0, 0, 200), self.rect)
     
-    def gravity(ground_y):
+    def gravity():
         self.rect.y += self.y_vel
         self.y_vel += self.y_momentum
 
-        if self.rect.y >= ground_y:
+        if self.rect.y >= self.limit:
             self.y_vel = 0
     
     def jump():
