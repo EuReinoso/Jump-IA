741b5cd4a2621e05f7fdb5206fb81756c86d147b
EuReinoso <lucas.reinoso.br@gmail.com>
2021-05-03 11:59:54 -0300

    score

--- population.py
+++ population.py
@@ -5,6 +5,8 @@ class Population:
     def __init__(self, population_size):
         self.population_size = population_size
         self.individuals = []
+        self.score = 0
+        self.best_score = 0
 
     def init(self,rect, network_sizes):
         for i in range(self.population_size):
@@ -28,6 +30,9 @@ class Population:
         for idv in self.individuals:
             if idv.collide(rect) and len(self.individuals) > 1:
                 self.individuals.remove(idv)
+    
+    def score_increment(self, vel):
+        self.score += vel
     
     def crossover(self, rect, network_sizes):
         idv1 = self.individuals[0]
@@ -54,11 +59,15 @@ class Population:
     def replication(self, rect, network_sizes):
         idv = self.individuals[0]
 
+        if self.score > self.best_score:
+            self.best_score = self.score
+
         self.individuals.clear()
         while len(self.individuals) < self.population_size:
             
             son = Individual(rect, network_sizes)
             son.network.weights = idv.network.weights
+            son.network.biases = idv.network.biases
             son.network.mutation()
 
             self.individuals.append(son)
