1a7dde85112403f387bdb5ba69f0d38c83fdc427
EuReinoso <lucas.reinoso.br@gmail.com>
2021-05-04 20:22:22 -0300

    improving random mutation

--- network.py
+++ network.py
@@ -1,5 +1,6 @@
 import numpy as np
-from random import random
+from random import random, seed
+from datetime import datetime
 
 def sigmoid(z):
     z = np.array(z, dtype= np.float128)
@@ -10,6 +11,9 @@ def threshold(z):
         return 1
     return 0
 
+def relu(z):
+    return np.maximum(z, 0)
+
 class Network:
     def __init__(self, sizes):
         self.num_layers = len(sizes)
@@ -19,18 +23,24 @@ class Network:
 
     def feedforward(self, a):
         for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w, a) + b)
+            #a = sigmoid(np.dot(w, a) + b)
             #a = threshold(np.dot(w, a) + b)
+            a = relu(np.dot(w,a) + b)
         return a
 
     def mutation(self):
-        if random() <= 0.05:
-            self.biases[0][0] = np.random.randn()
-            
+        for i in range(len(self.biases)):
+            for j in range(len(self.biases[i])):
+                for k in range(len(self.biases[i][j])):
+                    seed(datetime.now())
+                    if random() <= 0.05:
+                        self.biases[0][0] = np.random.randn()
+        
         for i in range(len(self.weights)):
             for j in range(len(self.weights[i])):
                 for k in range(len(self.weights[i][j])):
+                    seed(datetime.now())
                     if  random() <= 0.05:
-                        self.weights[i][j][k] = np.random.randn() 
+                        self.weights[i][j][k] = np.random.randn()
 
     
