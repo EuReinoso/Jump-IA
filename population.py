7b03c767e45ee94d3ec8607b2aa52f99894ccc98
EuReinoso <lucas.reinoso.br@gmail.com>
2021-05-04 20:41:52 -0300

    improving father selection

--- population.py
+++ population.py
@@ -6,11 +6,9 @@ class Population:
         self.population_size = population_size
         self.individuals = []
         self.score = 0
-
-        self.count_generations = 1
-
         self.best_score = 0
-        self.best_individual = None
+
+        self.count_generations = 0
         self.last_individual = None
 
     def init(self,rect, network_sizes):
@@ -35,52 +33,28 @@ class Population:
     def collide(self, rect):
         for idv in self.individuals:
             if idv.collide(rect) and len(self.individuals) > 0:
-                self.individuals.remove(idv)
-
                 if len(self.individuals) == 1:
                     self.last_individual = idv
+
+                self.individuals.remove(idv)
+
     
     def score_increment(self, vel):
         self.score += vel
-    
-    def crossover(self, rect, network_sizes):
-        idv1 = self.individuals[0]
-        idv2 = self.individuals[1]
-
-        self.individuals.clear()
-        while len(self.individuals) < self.population_size:
-            cut = round(random() * len(idv1.network.weights))
-
-            weights1 = idv1.network.weights[0:cut] + idv2.network.weights[cut::]
-            weights2 = idv2.network.weights[0:cut] + idv1.network.weights[cut::]
-
-            son1 = Individual(rect, network_sizes)
-            son1.network.weights = weights1
-            son1.network.mutation()
-
-            son2 = Individual(rect, network_sizes)
-            son2.network.weights = weights2
-            son2.network.mutation()
-
-            self.individuals.append(son1)
-            self.individuals.append(son2)
 
     def replication(self, rect, network_sizes):    
-        if self.score > self.best_score:
-            idv = self.last_individual
+        if self.score > self.best_score:  
             self.best_score = self.score
-        else:
-            idv = self.best_individual
         
+        idv = self.last_individual
         self.count_generations += 1
         self.score = 0
         self.individuals.clear()
+
         while len(self.individuals) < self.population_size:
-            
             son = Individual(rect, network_sizes)
             son.network.weights = idv.network.weights
             son.network.biases = idv.network.biases
             son.network.mutation()
-
             self.individuals.append(son)
 
